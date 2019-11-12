9d13af0fcf4fdd5faca84f0da69b7e4964fbf1f4
Bijay Karki <n01271571@nj-00118-019.humber.org>
2019-11-12 15:39:06 -0500

    js updated

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let calculate = document.getElementById("Calculate");
+
+function loancalculation ()
+{
+    let loan = document.getElementById("amount").value; 
+    let years = document.getElementById("years").value;
+    console.log(loan ,years);
+    const rate= 5;
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+calculate.addEventListener("click", loancalculation);
