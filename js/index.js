0ac61f6cdba5f4f1760bbe74456899502527763e
bijaybdrkarki <bijay.jhupro@gmail.com>
2019-11-19 12:09:10 -0500

    js updated

--- js/index.js
+++ js/index.js
@@ -6,9 +6,18 @@ function loancalculation ()
     let years = document.getElementById("years").value;
     console.log(loan ,years);
     const rate= 5;
-
+    let numOfpayments = rate * 12;
+    let monthlyInterest = 1 + (rate/100)/12;
+    let calculation = 1 - Math.pow(monthlyInterest, -numOfpayments);
     
+    let monthlyloan = loan * (monthlyInterest-1);
     
+    let monthlyInstallment = monthlyloan/calculation;
+    console.log(monthlyInstallment);
+    document.getElementById("loan").innerHTML = loan;
+    document.getElementById("rates").innerHTML += rate;
+    document.getElementById("year").innerHTML += years;
+    document.getElementById("installment").innerHTML += monthlyInstallment.toFixed(2);
 }
 
 
